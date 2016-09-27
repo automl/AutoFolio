21bd5bc8353b46dd54d1ef8bfc86a4cc24ad1472
mlindauer <lindauer@informatik.uni-freiburg.de>
2016-09-27 10:43:31 +0200

    ADD setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+import setuptools
+
+
+requirements = ['ConfigSpace', 'numpy', 'scipy', 'scikit-learn', 'pyyaml', 'liac-arff', 'pandas', 'smac']
+
+setuptools.setup(
+    name="autofolio",
+    version="1.0.0",
+    author="Marius Lindauer",
+    author_email="lindauer@cs.uni-freiburg.de",
+    description=("AutoFolio 2, an automaticalliy configured algorithm selector."),
+    license="3-clause BSD",
+    keywords="algortithm selection",
+    url="",
+    packages=setuptools.find_packages(exclude=['test', 'source']),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: 3-clause BSD",
+    ],
+    platforms=['Linux'],
+    install_requires=requirements,
+    tests_require=['mock',
+                   'nose'],
+    test_suite='nose.collector'
+)
