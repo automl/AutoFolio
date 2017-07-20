d87d1a3c9f21c81d0cbd045a8829b3bd6df5a11b
mlindauer <marius.rks@googlemail.com>
2017-07-20 13:43:25 +0200

    Cleanung of data

--- autofolio/selector/multiclass_classifiers/dnn.py
+++ autofolio/selector/multiclass_classifiers/dnn.py
@@ -13,7 +13,7 @@ from ConfigSpace import Configuration
 
 from aslib_scenario.aslib_scenario import ASlibScenario
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 import keras
 from keras import backend as K
@@ -52,6 +52,7 @@ class DNN(object):
 
         self.model = None
         self.scaler = StandardScaler()
+        self.logger = logging.getLogger("DNN")
         
     def __str__(self):
         return "DNN"
@@ -70,7 +71,28 @@ class DNN(object):
                 configuration
 
         '''
+        MAX_EPOCHS = 2000
+        
         logging.basicConfig(level="INFO")
+        
+        n_shape_init = X.shape[0]
+        X = pd.DataFrame(X)
+        Y = pd.DataFrame(Y)
+
+        # remove common timeouts        
+        all_timeouts = Y.min(axis=1) >= 600
+        X.drop(Y[all_timeouts].index, inplace=True)
+        Y.drop(Y[all_timeouts].index, inplace=True)
+        
+        # remove duplicates
+        dups = X.duplicated()
+        Y.drop(X[dups].index, inplace=True)
+        X.drop(X[dups].index, inplace=True)
+        
+        X = X.values
+        Y = Y.values
+        
+        self.logger.debug("Dropped %d observations because of duplicates and common timeouts." %(n_shape_init - X.shape[0]))
         
         X = self.scaler.fit_transform(X)
         
@@ -92,23 +114,36 @@ class DNN(object):
                                               max_num_epochs=500,
                                               metrics=[as_loss],
                                               verbose=1)
-        else:
+        elif True:
             Y_sel = np.argmin(Y, axis=1)
-            Y_hot = keras.utils.to_categorical(Y_sel, num_classes=Y.shape[0])
+            Y_hot = keras.utils.to_categorical(Y_sel, num_classes=Y.shape[1])
             Y = Y_hot
             
             an = AutoNet(max_layers=5, n_classes=Y.shape[1])
-            config = an.fit(X_train=X, y_train=Y, X_valid=X, y_valid=Y, max_epochs=50, 
-                            runcount_limit=50)
+            config = an.fit(X_train=X, y_train=Y, X_valid=X, y_valid=Y,
+                            max_epochs_intensify=100, max_epochs=MAX_EPOCHS, 
+                            runcount_limit=200)
             
             pc = ParamFCNetClassification(config=config, n_feat=X.shape[1],
                                   n_classes=Y.shape[1],
-                                  max_num_epochs=500,
+                                  max_num_epochs=MAX_EPOCHS,
+                                  verbose=1)
+        else:
+            Y_sel = np.argmin(Y, axis=1)
+            Y_hot = keras.utils.to_categorical(Y_sel, num_classes=Y.shape[1])
+            Y = Y_sel
+            cs = ParamFCNetClassification.get_config_space(max_num_layers=5,
+                                                       use_l2_regularization=False,
+                                                       use_dropout=False)
+            config = cs.get_default_configuration()
+            pc = ParamFCNetClassification(config=config, n_feat=X.shape[1],
+                                  n_classes=Y_hot.shape[1],
+                                  max_num_epochs=100,
                                   verbose=1)
             
         
         history = pc.train(X_train=X, y_train=Y, X_valid=X,
-                           y_valid=Y, n_epochs=500)
+                           y_valid=Y, n_epochs=MAX_EPOCHS)
 
         self.model = pc.model
         
