497ca7a63b6b6009a35ce02edf7b4ea065dd4d8e
Marius Lindauer <marius.rks@googlemail.com>
2020-03-14 15:02:40 +0100

    ADD a lot of scripts for AutoDL

---
+++ examples/autodl/example_predict.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from autofolio.facade.af_csv_facade import AFCsvFacade
+
+__author__ = "Marius Lindauer"
+__license__ = "BSD"
+__version__ = "2.1.0"
+
+perf_fn = "perf_matrix.csv"
+feat_fn = "meta_features.csv"
+
+# will be created (or overwritten) by AutoFolio
+model_fn = "af_model_final.pkl"
+
+# load AutoFolio model and
+# get predictions for new meta-feature vector
+pred = AFCsvFacade.load_and_predict(vec=np.array([3.,102., 6149., -1.,]), load_fn=model_fn)
+
+print(pred)
+
+
