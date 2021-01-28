9b989d046f9a9a400e528c356b5ad901209ac512
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2021-01-28 16:29:44 +0100

    Fix issue where PCA would fail when n_components was chosen larger than the number of instances or features

--- autofolio/feature_preprocessing/pca.py
+++ autofolio/feature_preprocessing/pca.py
@@ -20,14 +20,20 @@ __license__ = "BSD"
 class PCAWrapper(object):
 
     @staticmethod
-    def add_params(cs: ConfigurationSpace):
+    def add_params(cs: ConfigurationSpace, scenario: ASlibScenario):
         '''
             adds parameters to ConfigurationSpace 
         '''
         pca_switch = CategoricalHyperparameter(
             "pca", choices=[True, False], default_value=False)
+        n_instances = len(scenario.instances)
+        n_features = len(scenario.features)
+        n_comp_upper_default = 20
+        n_comp_upper = min(n_comp_upper_default, n_instances, n_features)
+        n_comp_value_default = 7
+        n_comp_value = min(n_comp_value_default, n_comp_upper)
         n_components = UniformIntegerHyperparameter(
-            "pca_n_components", lower=1, upper=20, default_value=7, log=True)
+            "pca_n_components", lower=1, upper=n_comp_upper, default_value=n_comp_value, log=True)
         cs.add_hyperparameter(pca_switch)
         cs.add_hyperparameter(n_components)
         cond = InCondition(
